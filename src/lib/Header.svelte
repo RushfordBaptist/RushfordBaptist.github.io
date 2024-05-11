25c416937bd89c2af82f25dc7be133dda9cc7857
Patrick Barringer <patrick.barringer@gmail.com>
2024-05-10 21:54:34 -0400

    Playing with Header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let imageUrl;
+	export let title;
+</script>
+
+<div class="header" style="background-image: url({imageUrl});">
+	<h1>{title}</h1>
+</div>
+
+<style>
+	.header {
+		background-size: cover;
+		background-position: center;
+		height: 200px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: white;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+	import Header from '$lib/Header.svelte';
+</script>
+
+<Header imageUrl="https://i.imgur.com/CBNhtBo.jpg" title="Test" />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	:global(body) {
+		margin: 0;
+	}
+</style>
