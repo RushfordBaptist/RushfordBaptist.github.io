1345986b7714d0494af89b24798541c3978267cc
Patrick Barringer <patrick.barringer@gmail.com>
2024-09-07 21:02:53 -0400

    Added title to calendar iframe

--- src/lib/GoogleCalendar.svelte
+++ src/lib/GoogleCalendar.svelte
@@ -1,4 +1,5 @@
 <iframe
+	title="Rushford Baptist Church calendar"
 	src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&showPrint=0&showTabs=0&showCalendars=0&showTz=0&showTitle=0&src=cnVzaGZvcmRiYXB0aXN0Y2h1cmNoQGdtYWlsLmNvbQ&color=%23039BE5"
 	frameborder="0"
 	scrolling="no"
